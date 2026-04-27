23974d61b4e6c503e9c2c20b1f0be1e25a410358
Raj Kumar Nepal <nplrajk@gmail.com>
2026-04-27 15:57:08 +0545

    feat(core): added curr_sec field to AsmCtx for section-aware assembly

--- include/type.s
+++ include/type.s
@@ -167,7 +167,8 @@ struc ASMCTX
     field ninc_paths,  4       // include path count
     field flags,       4       // CTX_FLAG_* values
     field last_global, 8       // pointer to string of last global label
-    field pad2,        16      // future reservation
+    field curr_sec,    8       // pointer to current active SECTION
+    field pad2,        8       // future reservation
 endstruc
 
 // ============================================================================
